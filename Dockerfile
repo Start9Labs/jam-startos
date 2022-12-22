a2858ffe0a0fa3a52a1cf9b6713e540f0c28203b
Dread <34528298+islandbitcoin@users.noreply.github.com>
2022-12-21 21:27:31 -0500

    Initial Release (#2)
    * refactored for x86_64 architecture
    
    * updated to v0.1.4
    
    * bugfixes
    
    Co-authored-by: islandbitcoin <dread@start9.com>

--- Dockerfile
+++ Dockerfile
@@ -1,37 +1,22 @@
-FROM ghcr.io/joinmarket-webui/jam-standalone:latest
-RUN apt-get update && apt-get install -qq --no-install-recommends wget bash
-RUN wget https://github.com/mikefarah/yq/releases/download/v4.12.2/yq_linux_arm.tar.gz -O - |\
-      tar xz && mv yq_linux_arm /usr/bin/yq
+FROM ghcr.io/joinmarket-webui/jam-standalone:v0.1.4-clientserver-v0.9.8
 
-USER root
+# arm64 or amd64
+ARG PLATFORM
+# aarch64 or x86_64
+ARG ARCH
 
-ENV DATADIR /root/.joinmarket
-ENV CONFIG ${DATADIR}/joinmarket.cfg
-ENV DEFAULT_CONFIG /default.cfg
-ENV JM_RPC_HOST="bitcoind.embassy"
-ENV JM_RPC_PORT="8332"
-ENV JM_RPC_USER="bitcoin"
-ENV JM_RPC_PASSWORD=
+RUN apt-get update && apt-get install -qq --no-install-recommends wget bash tini
+RUN wget -qO /usr/local/bin/yq https://github.com/mikefarah/yq/releases/latest/download/yq_linux_${PLATFORM} && chmod +x /usr/local/bin/yq
+
+# USER root
+
 ENV APP_USER "joinmarket"
 ENV APP_PASSWORD "joinmarket"
 ENV ENSURE_WALLET true
-ENV JM_NETWORK mainnet
+
 ENV REMOVE_LOCK_FILES true
 ENV RESTORE_DEFAULT_CONFIG true
 
-ADD jam-docker/standalone/nginx/ /nginx/
-ADD jam-docker/standalone/default.cfg /
-ADD jam-docker/standalone/torrc /
-
-RUN chmod a+x /default.cfg
-RUN chmod a+x /torrc
-
 ADD docker_entrypoint.sh /usr/local/bin/docker_entrypoint.sh
-ADD assets/utils/check-web.sh /usr/local/bin/check-web.sh
 ADD assets/utils/check-api.sh /usr/local/bin/check-api.sh
-ADD actions/unlock-wallet.sh /usr/local/bin/unlock-wallet.sh
 RUN chmod a+x /usr/local/bin/*.sh
-
-EXPOSE 80 28183 27183 8080
-
-ENTRYPOINT ["/usr/local/bin/docker_entrypoint.sh"]
--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -2,73 +2,37 @@
 
 set -e
 
-_term() { 
-  echo "Caught SIGTERM signal!"
-  kill -TERM "$jm_child" 2>/dev/null
-  exit 0
-}
-
 # Setting env-vars
 echo "Setting environment variables..."
-export APP_USER=$(yq e '.username' /root/start9/config.yaml)
-export APP_PASSWORD=$(yq e '.password' /root/start9/config.yaml)
-export TOR_HOST=$(yq e '.tor-address' /root/start9/config.yaml)
-export LAN_HOST=$(yq e '.lan-address' /root/start9/config.yaml)
-export RPC_TYPE=$(yq e '.bitcoind.type' /root/start9/config.yaml)
-export JM_RPC_USER=$(yq e '.bitcoind.user' /root/start9/config.yaml)
-export JM_RPC_PASSWORD=$(yq e '.bitcoind.password' /root/start9/config.yaml)
+export APP_USER=$(yq e '.username' /embassy/start9/config.yaml)
+export APP_PASSWORD=$(yq e '.password' /embassy/start9/config.yaml)
+export TOR_HOST=$(yq e '.tor-address' /embassy/start9/config.yaml)
+export LAN_HOST=$(yq e '.lan-address' /embassy/start9/config.yaml)
+export JM_RPC_USER=$(yq e '.bitcoind-user' /embassy/start9/config.yaml)
+export JM_RPC_PASSWORD=$(yq e '.bitcoind-password' /embassy/start9/config.yaml)
 export JM_RPC_PORT=8332
-export JM_WALLET="embassy_jam_wallet"
-export JM_HOST="jam.embassy"
-export JM_WALLET_RPC_USER=$(yq e '.wallet-rpc-user' /root/start9/config.yaml)
-export JM_WALLET_RPC_PASSWORD=$(yq e '.wallet-rpc-password' /root/start9/config.yaml)
-export JM_WALLET_RPC_HOST="bitcoind.embassy"
-
-if [ "$RPC_TYPE" = "internal-proxy" ]; then
-	export JM_RPC_HOST="btc-rpc-proxy.embassy"
-	echo "Running on Bitcoin Proxy..."
-else
-	export JM_RPC_HOST="bitcoind.embassy"
-	echo "Running on Bitcoin Core..."
-fi
-
-if ! [ -f "/root/.joinmarket/joinmarket.cfg" ]; then
-  # Create Core Wallet using the Bitcoin Core Credentials. Trying to create a non descriptor wallet using the proxy credentials results in an error
-  echo "Creating Default Wallet..."
-  curl -sS --user $JM_WALLET_RPC_USER:$JM_WALLET_RPC_PASSWORD --data-binary '{"jsonrpc": "1.0", "id": "wallet-gen", "method": "createwallet", "params": {"wallet_name":"'$JM_WALLET'","descriptors":false}}' -H 'content-type: text/plain;' http://$JM_WALLET_RPC_HOST:8332/
-fi
-
-# # Configuring Webserver
-echo "Configuring JoinMarket..."
-sed -i "s/rpc_host =.*/rpc_host = $JM_RPC_HOST/" /default.cfg
-sed -i "s/rpc_user =.*/rpc_user = $JM_RPC_USER/" /default.cfg
-sed -i "s/rpc_port =.*/rpc_port = $JM_RPC_PORT/" /default.cfg
-sed -i "s/rpc_password =.*/rpc_password = $JM_RPC_PASSWORD/" /default.cfg
-sed -i "s/rpc_wallet_file =.*/rpc_wallet_file = $JM_WALLET/" /default.cfg
-sed -i "s/jm_webui_default/$JM_WALLET/" /jam-entrypoint.sh
+export JM_RPC_WALLET_FILE="embassy_jam_wallet"
+export JM_RPC_HOST="bitcoind.embassy"
+echo "Running on Bitcoin Core..."
 
 # Properties Page showing password to be used for login
-  echo 'version: 2' > /root/start9/stats.yaml
-  echo 'data:' >> /root/start9/stats.yaml
-  echo '  Username: ' >> /root/start9/stats.yaml
-        echo '    type: string' >> /root/start9/stats.yaml
-        echo "    value: \"$APP_USER\"" >> /root/start9/stats.yaml
-        echo '    description: This is your username for JAM' >> /root/start9/stats.yaml
-        echo '    copyable: true' >> /root/start9/stats.yaml
-        echo '    masked: false' >> /root/start9/stats.yaml
-        echo '    qr: false' >> /root/start9/stats.yaml
-  echo '  Password: ' >> /root/start9/stats.yaml
-        echo '    type: string' >> /root/start9/stats.yaml
-        echo "    value: \"$APP_PASSWORD\"" >> /root/start9/stats.yaml
-        echo '    description: This is your password for JAM. Please use caution when sharing this password, you could lose your funds!' >> /root/start9/stats.yaml
-        echo '    copyable: true' >> /root/start9/stats.yaml
-        echo '    masked: true' >> /root/start9/stats.yaml
-        echo '    qr: false' >> /root/start9/stats.yaml
+  echo 'version: 2' > /embassy/start9/stats.yaml
+  echo 'data:' >> /embassy/start9/stats.yaml
+  echo '  Username: ' >> /embassy/start9/stats.yaml
+        echo '    type: string' >> /embassy/start9/stats.yaml
+        echo "    value: \"$APP_USER\"" >> /embassy/start9/stats.yaml
+        echo '    description: This is your username for JAM' >> /embassy/start9/stats.yaml
+        echo '    copyable: true' >> /embassy/start9/stats.yaml
+        echo '    masked: false' >> /embassy/start9/stats.yaml
+        echo '    qr: false' >> /embassy/start9/stats.yaml
+  echo '  Password: ' >> /embassy/start9/stats.yaml
+        echo '    type: string' >> /embassy/start9/stats.yaml
+        echo "    value: \"$APP_PASSWORD\"" >> /embassy/start9/stats.yaml
+        echo '    description: This is your password for JAM. Please use caution when sharing this password, you could lose your funds!' >> /embassy/start9/stats.yaml
+        echo '    copyable: true' >> /embassy/start9/stats.yaml
+        echo '    masked: true' >> /embassy/start9/stats.yaml
+        echo '    qr: false' >> /embassy/start9/stats.yaml
 
 # Starting JoinMarket API
 echo "Starting JoinMarket..."
-cd /src/scripts/
-/jam-entrypoint.sh
-
-trap _term SIGTERM
-wait $jm_child
+exec /jam-entrypoint.sh 
