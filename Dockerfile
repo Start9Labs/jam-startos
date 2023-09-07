4535f43b1b4765603b0019b11dec517adb2c4150
gStart9 <george@start9labs.com>
2023-09-06 22:00:49 -0600

    Unprivileged users mode

--- Dockerfile
+++ Dockerfile
@@ -5,19 +5,27 @@ ARG PLATFORM
 # aarch64 or x86_64
 ARG ARCH
 
+USER root
+
 RUN sed -i "s|http://|https://|g" /etc/apt/sources.list /etc/apt/sources.list.d/* 
-RUN apt-get update && apt-get -y upgrade && apt-get install -y -qq --no-install-recommends wget bash tini
+RUN apt-get update && apt-get upgrade -y --no-install-recommends && apt-get install -y -qq --no-install-recommends wget bash tini sudo curl
 RUN wget -qO /usr/local/bin/yq https://github.com/mikefarah/yq/releases/latest/download/yq_linux_${PLATFORM} && chmod +x /usr/local/bin/yq
 
-# USER root
+RUN groupadd -g 701234 jm
+RUN useradd -u 7012347 -g 701234 -s /bin/nologin -d /data jm
+RUN useradd -u 7012348 -g 701234 -s /bin/nologin -d /data jm-ob
 
-ENV APP_USER "joinmarket"
-ENV APP_PASSWORD "joinmarket"
 ENV ENSURE_WALLET true
-
 ENV REMOVE_LOCK_FILES true
-ENV RESTORE_DEFAULT_CONFIG true
+ENV RESTORE_DEFAULT_CONFIG false
 
 ADD docker_entrypoint.sh /usr/local/bin/docker_entrypoint.sh
 ADD assets/utils/check-api.sh /usr/local/bin/check-api.sh
 RUN chmod a+x /usr/local/bin/*.sh
+
+#Run the joinmarket snakes as unprivileged users and ensure correct privileges:
+RUN sed -i "s|^command = \(python3 jmwalletd.py\)$|command = sudo -u jm \1|" /etc/dinit.d/jmwalletd
+RUN sed -i "s|^command = \(python3 ob-watcher.py --host=127.0.0.1\)$|command = sudo -u jm-ob \1|" /etc/dinit.d/ob-watcher
+#make sure the jm user's home directory exists &
+#make sure the jm user and jm group can write into the proper joinmarket dirs
+RUN sed -i "s|^\(exec /sbin/dinit --container\)$|mkdir -p /data/.joinmarket/\{cmtdata,logs,ssl,wallets\} \&\& chown -R jm:jm /data \&\& chmod 0775 /data/.joinmarket/\{cmtdata,logs,ssl\}\n\1|" /jam-entrypoint.sh
--- docker_entrypoint.sh
+++ docker_entrypoint.sh
@@ -4,35 +4,42 @@ set -e
 
 # Setting env-vars
 echo "Setting environment variables..."
-export APP_USER=$(yq e '.username' /embassy/start9/config.yaml)
-export APP_PASSWORD=$(yq e '.password' /embassy/start9/config.yaml)
-export TOR_HOST=$(yq e '.tor-address' /embassy/start9/config.yaml)
-export LAN_HOST=$(yq e '.lan-address' /embassy/start9/config.yaml)
-export JM_RPC_USER=$(yq e '.bitcoind-user' /embassy/start9/config.yaml)
-export JM_RPC_PASSWORD=$(yq e '.bitcoind-password' /embassy/start9/config.yaml)
+export APP_USER=$(yq e '.username' /data/start9/config.yaml)
+export APP_PASSWORD=$(yq e '.password' /data/start9/config.yaml)
+export TOR_HOST=$(yq e '.tor-address' /data/start9/config.yaml)
+export LAN_HOST=$(yq e '.lan-address' /data/start9/config.yaml)
+export JM_RPC_USER=$(yq e '.bitcoind-user' /data/start9/config.yaml)
+export JM_RPC_PASSWORD=$(yq e '.bitcoind-password' /data/start9/config.yaml)
 export JM_RPC_PORT=8332
-export JM_RPC_WALLET_FILE="embassy_jam_wallet"
+export JM_RPC_WALLET_FILE="jam_wallet"
 export JM_RPC_HOST="bitcoind.embassy"
-echo "Running on Bitcoin Core..."
+export DATADIR="/data/.joinmarket"
+export DEFAULT_CONFIG="/data/default.cfg"
+export CONFIG="/data/.joinmarket/joinmarket.cfg"
+#export LOGDIR="/data/.joinmarket/logs"
 
 # Properties Page showing password to be used for login
-echo 'version: 2' > /embassy/start9/stats.yaml
-echo 'data:' >> /embassy/start9/stats.yaml
-echo '  Username: ' >> /embassy/start9/stats.yaml
-echo '    type: string' >> /embassy/start9/stats.yaml
-echo "    value: \"$APP_USER\"" >> /embassy/start9/stats.yaml
-echo '    description: This is your username for JAM' >> /embassy/start9/stats.yaml
-echo '    copyable: true' >> /embassy/start9/stats.yaml
-echo '    masked: false' >> /embassy/start9/stats.yaml
-echo '    qr: false' >> /embassy/start9/stats.yaml
-echo '  Password: ' >> /embassy/start9/stats.yaml
-echo '    type: string' >> /embassy/start9/stats.yaml
-echo "    value: \"$APP_PASSWORD\"" >> /embassy/start9/stats.yaml
-echo '    description: This is your password for JAM. Please use caution when sharing this password, you could lose your funds!' >> /embassy/start9/stats.yaml
-echo '    copyable: true' >> /embassy/start9/stats.yaml
-echo '    masked: true' >> /embassy/start9/stats.yaml
-echo '    qr: false' >> /embassy/start9/stats.yaml
+echo 'version: 2' > /data/start9/stats.yaml
+echo 'data:' >> /data/start9/stats.yaml
+echo '  Username: ' >> /data/start9/stats.yaml
+echo '    type: string' >> /data/start9/stats.yaml
+echo "    value: \"$APP_USER\"" >> /data/start9/stats.yaml
+echo '    description: This is your username for JAM' >> /data/start9/stats.yaml
+echo '    copyable: true' >> /data/start9/stats.yaml
+echo '    masked: false' >> /data/start9/stats.yaml
+echo '    qr: false' >> /data/start9/stats.yaml
+echo '  Password: ' >> /data/start9/stats.yaml
+echo '    type: string' >> /data/start9/stats.yaml
+echo "    value: \"$APP_PASSWORD\"" >> /data/start9/stats.yaml
+echo '    description: This is your password for JAM. Please use caution when sharing this password, you could lose your funds!' >> /data/start9/stats.yaml
+echo '    copyable: true' >> /data/start9/stats.yaml
+echo '    masked: true' >> /data/start9/stats.yaml
+echo '    qr: false' >> /data/start9/stats.yaml
+
+#Copy in default config from docker image
+cp -f /root/default.cfg /data/default.cfg
 
 # Starting JoinMarket API
+echo ""
 echo "Starting JoinMarket..."
-exec /jam-entrypoint.sh 
+exec /jam-entrypoint.sh
