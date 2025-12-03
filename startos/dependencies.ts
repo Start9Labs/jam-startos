88abcca2a40bc1e2d31a96ce71b4663046a58137
Shadowy Super Coder <musashidisciple@proton.me>
2025-12-02 21:04:54 -0700

    <30.0 bitcoind

--- startos/dependencies.ts
+++ startos/dependencies.ts
@@ -3,7 +3,7 @@ import { sdk } from './sdk'
 export const setDependencies = sdk.setupDependencies(async ({ effects }) => ({
   bitcoind: {
     kind: 'running',
-    versionRange: '>=29.1:2-beta.0',
+    versionRange: '^29.1:2-beta.0', // jam currently uses bdb which is incompatible with Core v30
     healthChecks: ['sync-progress'],
   },
 }))
