8c47b2112a19d1617635701d3109f37be8130990
Shadowy Super Coder <musashidisciple@proton.me>
2025-12-09 16:20:02 -0700

    create joinmarket.cfg if it doesn't exist

--- startos/install/versions/v0.4.1.2-beta.0.ts
+++ startos/install/versions/v0.4.1.2-beta.0.ts
@@ -1,7 +1,9 @@
 import { VersionInfo, IMPOSSIBLE } from '@start9labs/start-sdk'
-import { readFile, rm } from 'fs/promises'
+import { readFile } from 'fs/promises'
 import { load } from 'js-yaml'
 import { store } from '../../fileModels/store.json'
+import { joinmarketCfg } from '../../fileModels/joinmarket.cfg'
+import { configDefaults } from '../../utils'
 
 export const v_0_4_1_2_beta0 = VersionInfo.of({
   version: '0.4.1:2-beta.0',
@@ -23,9 +25,15 @@ export const v_0_4_1_2_beta0 = VersionInfo.of({
         await store.write(effects, { password: yaml.password })
       }
 
-      await rm('/media/startos/volumes/main/start9', { recursive: true }).catch(
-        console.error,
-      )
+      const conf = await joinmarketCfg.read().once()
+
+      if (!conf) {
+        console.log("No existing joinmarket.cfg. Writing defaults...")
+        await joinmarketCfg.write(effects, configDefaults)
+      } else {
+        console.log("Using existing joinmarket.cfg")
+      }
+
     },
     down: IMPOSSIBLE,
   },
